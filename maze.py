e2403471afdefce6bbbba480c0d1cd759acd1938
speedyjeff <speedyjeff@github.com>
2026-01-04 09:56:30 -0800

    additional maze optimizations - currently ranked ~325 in leaderboard (though single drone)

--- maze.py
+++ maze.py
@@ -2,27 +2,24 @@
 # primary features:
 #  depth first search to explore maze and build a tree
 #  point to origin pathing (for quick paths without searches)
-#  path pruning to find closer points of intersection
+#  finding common pivot point in paths to avoid backtracking
 #  option to change world size (smaller may offer a better balance of speed and chest payout)
 #  every reexplore_rate times the maze is reexplored to find faster paths
 # todo -
-#  two paths are generated every turn, BUT the exact same path (but reverse) is needed for the next round
-#   so reuse that path (faster?)
 #  the tree operates as a single parent tree, but that is not true as the maze evolves
-#  dfs init, on clear, is slow
+#   once the maze evolves it is more like a graph and less like a tree
 start_over = True
 on_loop = True
-reexplore_rate = 50
-size = get_world_size()
-world_size = 33
+is_simulation = False
+reexplore_rate = 100
+world_size = 33 # get_world_size()
 substance = world_size * 2**(num_unlocked(Unlocks.Mazes) - 1)
 
-max_int = 2147483648
 tree = []
 max_parent = -1
-directions = [North,South,East,West]
-opposite_direction = {North:South,South:North,East:West,West:East}
-coords = [(1,0),(-1,0),(0,1),(0,-1)]
+# None is parent
+opposite_direction = {None:None,North:South,South:North,East:West,West:East}
+coordinates = {North:(1,0),South:(-1,0),East:(0,1),West:(0,-1)}
 
 #
 # convert an y,x world coordinate to a 1D array index
@@ -50,6 +47,7 @@ def dfs_init():
 	if len(tree) > 0:
 		# clear
 		for i in range(len(tree)):
+			tree[i][None] = None
 			tree[i][North] = 0
 			tree[i][South] = 0
 			tree[i][East] = 0
@@ -58,7 +56,7 @@ def dfs_init():
 		# generate the maze tree structure (dfs)
 		for x in range(world_size):
 			for y in range(world_size):
-				tree.append({North: 0, South: 0, East: 0, West: 0})
+				tree.append({None:None,North: 0, South: 0, East: 0, West: 0})
 
 #
 # depth first search of the entire maze (do not collect the treasure)
@@ -72,6 +70,9 @@ def dfs_explore(parent):
 	if parent != None:
 		dir = opposite_direction[parent]
 		tree[index][dir] = max_parent
+		# set the first parent seen, avoid cycles
+		if tree[index][None] == None:
+			tree[index][None] = dir
 
 	# find unvisited neighbors
 	neighbors = []
@@ -122,104 +123,99 @@ def dfs_explore(parent):
 
 # 
 # compute the path back to the origin (using parent information)
-#  is_from_home: need to flip the directions (eg going the opposite direction to current location)
-#  prune: find parts of the path that are duplicates (eg do not need to go home every time)
 #
-def dfs_back_home(y,x,is_from_home,path):
-	path_index = 0
+def get_path_home(y,x):
+	path = []
 	while True:
 		if x == 0 and y == 0:
 			break
 		index = get_index(y,x)
-		# go to parent
-		found = False
-		for i in range(len(directions)):
-			if tree[index][directions[i]] == max_parent:
-				# go this direction
-				y += coords[i][0] 
-				x += coords[i][1]
-				dir = directions[i]
-				if is_from_home:
-					dir = opposite_direction[dir]
-				path.insert(path_index, (dir,y,x))
-				found = True
-				if not is_from_home:
-					path_index += 1
-				break
-		if not found:
+		dir = tree[index][None]
+		if dir == None:
 			print("failed to find parent")
-	
-	if path_index > 0:
-		# remove overlapping parts of the path, starting at path_index
-		start = path_index - 1
-		end = path_index
-		length = len(path)
-		# find all the matches y = [1], x = [2]
-		matches = 0
-		while start >= 0 and end < length and path[start][1] == path[end][1] and path[start][2] == path[end][2]:
-			start -= 1
-			end += 1
-			matches += 1
-		# the junction point is always a match, so subtract that
-		matches -= 1
-		start += 2
-		if matches > 0:
-			# add a prune hint to the path
-			path.append((None,start,matches*2))
-
+		else:
+			# go this direction
+			y += coordinates[dir][0] 
+			x += coordinates[dir][1]
+			path.append((dir,y,x))
+			
 	return path
 
 #
-# get the path back home and then to the destination
+# identify the common part of these two paths (may be origin)
 #
-def dfs_get_path(dy,dx):
-	# dx destination
-	# dy destination
-	# path to append too
-	# return a stack of moves to the next destination
-	x = get_pos_x()
-	y = get_pos_y()
-	
-	# traverse from the destination back home (but insert forward movement)
-	path = dfs_back_home(dy,dx,True,[])
-	
-	# traverse parents back up to home
-	path = dfs_back_home(y,x,False,path)
+def find_pivot_point(path1,path2):
+	# indicate overlapping parts of the paths - find the pivot point
+	# the end of the paths is origin, so compare the ends of these paths
+	i1 = len(path1) - 1
+	i2 = len(path2) - 1
+	# find all the matches y = [1], x = [2]
+	matches = 0
+	while i1 >= 0 and i2 >= 0 and path1[i1][1] == path2[i2][1] and path1[i1][2] == path2[i2][2]:
+		i1 -= 1
+		i2 -= 1
+		matches += 1
+	# the junction point is always a match, so subtract that
+	matches -= 1
+	i1 += 2
+	i2 += 1	
 
-	return path
+	if matches > 0:
+		return (matches, i1, i2)
+	else:
+		return (0,-1,-1)
 
 #
-# follow the path
-#  prune: prunning information is encoded as a None element which is last in the array
-#
-def dfs_traverse_path(path,get_treasure):
+# follow the path away from origin
+#  pivot_start: starting point of the pivot
+# 
+def traverse_away_from_origin(path,pivot_start):
+	# traverse against the stream of the path away from origin (eg starting point is origin*)
 	length = len(path)
 	if length > 0:
+		i = length - 1
 		# check for prunning
-		start = -1
-		skip = 0
-		if path[length-1][0] == None:
-			start = path[length-1][1]
-			skip = path[length-1][2]
+		if pivot_start >= 0:
+			i = pivot_start
+		# follow these directions
+		while i >= 0:
+			tup = path[i]
+			dir = tup[0]
+			# get the opposite direction
+			dir = opposite_direction[dir]
+			if dir != None and not move(dir):
+				print("failed to move")
+			i -= 1
+			
+	if not collect_treasure():
+		print("failed to get treasure")
+
+# 
+# follow the path towards origin
+#  pivot_start: starting point of the pivot_start
+#  skip: number of paths entries to skip
+#
+def traverse_to_origin(path,pivot_start,skip):
+	# traverse the path in the direction stored (eg follow parent info)
+	length = len(path)
+	if length > 0:
 		# follow these directions
 		i = 0
 		while i < length:
 			tup = path[i]
 			dir = tup[0]
-			if i == start:
-				i += skip
+			if i == pivot_start:
+				i += 1 + skip
 			else:
 				if dir != None and not move(dir):
 					print("failed to move")
 				i += 1
-			
-	if get_treasure and not collect_treasure():
-		print("failed to get treasure")
 
 #
 # init, explore, and run 300 times
 #
-while True:
+done = False
+while not done:
 	if start_over:
 		clear()
 		till()
@@ -232,20 +228,33 @@ while True:
 	# explore the maze
 	dfs_explore(None)
 	
+	from_current = []
+	from_treasure = []
 	for i in range(300):
 		if reexplore_rate > 0 and i > 0 and i % reexplore_rate == 0:
 			# go home
-			y = get_pos_y()
-			x = get_pos_x()
-			path = dfs_back_home(y,x,False,[])
-			dfs_traverse_path(path,False)
+			traverse_to_origin(from_current,-1,0)
 			# reexplore the maze
 			dfs_init()
 			dfs_explore(None)
+			from_current = []
 		
-		# to treasure (first go home)
+		# get path to treasure
 		x,y = measure()
-		path = dfs_get_path(y,x)
+		from_treasure = get_path_home(y,x)
 		
-		# move to the destination
-		dfs_traverse_path(path,True)	
+		# find the common pivot point
+		matches,current_start,treasure_start = find_pivot_point(from_current,from_treasure)
+		
+		# move to treasure
+		traverse_to_origin(from_current,current_start,matches)
+		traverse_away_from_origin(from_treasure,treasure_start)
+
+		# set path home
+		from_current = from_treasure
+		from_treasure = []
+		
+		# when simulating
+		if is_simulation and num_items(Items.Gold) >= 9863168:
+			done = True
+			break
